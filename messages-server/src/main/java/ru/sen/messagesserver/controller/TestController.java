986cf9aa4038c6e840f12367473d7842be8a6231
dkrasilnikov <dkrasilnikov@gear-games.com>
2023-02-21 21:22:12 +0300

    added a server call via routes parameters

--- messages-server/src/main/java/ru/sen/messagesserver/controller/TestController.java
+++ messages-server/src/main/java/ru/sen/messagesserver/controller/TestController.java
@@ -1,13 +1,15 @@
 package ru.sen.messagesserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/messages")
 public class TestController {
 
     @GetMapping("/test")
     public String testMethod() {
-        return "Hello!";
+        return "Hello! I am messages server";
     }
 }
